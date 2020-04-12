00fa11724353663d9889f7b902d7dad1f47899c3
Mikołaj Kania <mikolajkania@users.noreply.github.com>
2020-04-12 21:40:56 +0200

    Final experiment using Java 13

--- src/main/java/pl/itblues/JavaVersions.java
+++ src/main/java/pl/itblues/JavaVersions.java
@@ -5,8 +5,23 @@ import org.apache.bcel.Repository;
 public class JavaVersions {
 
     public static void main(String[] args) throws InterruptedException, ClassNotFoundException {
+        // var is available since Java 10
         var c = Repository.lookupClass("pl.itblues.JavaVersions");
-        System.out.println("Java version= " + c.getMajor());
+
+        String msg = "   Compiled Java version= " + c.getMajor();
+        System.out.println(msg);
+        System.out.println("Runtime Java version= " + System.getProperty("java.version"));
+
+        // strip method is available since Java 11, but can't be used even with Jabel
+        // System.out.println(msg.strip());
+
+        // text blocks can be used since Java 13
+        System.out.println(
+                        """
+                        From Gdańsk
+                        with love!
+                        """);
+
 
         Thread.sleep(10_000);
     }
